f037eb202711f3553b0fc094b46befd4eb78fb69
march-dave <ysangy@gmail.com>
2016-06-07 08:10:05 -0700

    replace web site template

---
+++ public/js/scrolling.js
@@ -0,0 +1,24 @@
+
+$(function () {
+    $(window).on("load resize", function () {
+        $(".fill-screen").css("height", window.innerHeight);
+    });
+
+    // add Bootstrap's scrollspy
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: 160
+    });
+
+    // smooth scrolling
+    $('nav a, .down-button a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // initialize WOW for element animation
+    new WOW().init();
+
+});
